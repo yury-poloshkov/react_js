178ae359b87b2c4e5ad4824e068939cbd99e4787
yury-poloshkov <1286600@gmail.com>
2024-09-24 06:17:09 +0300

    homework03 TemperatureConverter Completed

--- homework03/src/App.js
+++ homework03/src/App.js
@@ -10,7 +10,7 @@ function App() {
     ];
     return (
         <div className="App">
-            <TemperatureConverter temperature={20} />
+            <TemperatureConverter />
             <ToDoList comments={data} />
         </div>
     );
--- homework03/src/components/TemperatureConverter/TemperatureConverter.js
+++ homework03/src/components/TemperatureConverter/TemperatureConverter.js
@@ -1,12 +1,59 @@
 import "./TemperatureConverter.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-function TemperatureConverter({ temperature }) {
+function TemperatureConverter() {
+    const [celsium, setCelsium] = React.useState();
+    const [fahrenheit, setFahrenheit] = React.useState();
+
+    const inputCelsium = (event) => {
+        setCelsium(event.target.value);
+        setFahrenheit(event.target.value * 1.8 + 32);
+        console.log(fahrenheit);
+    };
+
+    const inputFahrenheit = (event) => {
+        setFahrenheit(event.target.value);
+        setCelsium((event.target.value - 32) / 1.8);
+        console.log(celsium);
+    };
+
     return (
-        <div className="Message">
-            <h2 className="Message__header">Температурный конвертер:</h2>
-            <p className="Message__text">Градусов Цельсия: {temperature}</p>
-            <p className="Message__text">Градусов Фаренгейта: {+temperature * 1.8 + 32}</p>
-        </div>
+        // FAHRENGHEIT = CELSIUM * 1.8 + 32
+
+        <Box component="form" sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }} noValidate autoComplete="off">
+            <div className="TemperatureConverter">
+                <h2 className="Message__header">Температурный конвертер:</h2>
+                <TextField
+                    id="outlined-number-celsium"
+                    label="Градусы по ЦЕЛЬСИЮ"
+                    type="number"
+                    slotProps={{
+                        inputLabel: {
+                            shrink: true,
+                        },
+                    }}
+                    color="secondary"
+                    onChange={inputCelsium}
+                    value={celsium}
+                />
+                <h2> = </h2>
+                <TextField
+                    id="outlined-number-fahrengheit"
+                    label="Градусы по ФАРЕНГЕЙТУ"
+                    type="number"
+                    slotProps={{
+                        inputLabel: {
+                            shrink: true,
+                        },
+                    }}
+                    color="secondary"
+                    onChange={inputFahrenheit}
+                    value={fahrenheit}
+                />
+            </div>
+        </Box>
     );
 }
 
