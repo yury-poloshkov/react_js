8d4631757a065112c79e79ad5a26c910103a7e90
yury-poloshkov <1286600@gmail.com>
2024-10-18 01:19:09 +0300

    homework05 refactored

--- homework05/src/components/MainPage.js
+++ homework05/src/components/MainPage.js
@@ -2,16 +2,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleTheme } from "../store/themeReducer";
 
 const MainPage = () => {
-    const { theme } = useSelector((state) => state.theme);
+    const theme = useSelector((state) => state.theme);
     const dispatch = useDispatch();
 
-    console.log(theme + " from MainPage Coponent");
     return (
-        <div className="App App-light">
+        <div className={"App App-" + theme}>
             <header>
                 <h1>HELLO WORLD!!!</h1>
-                <button onClick={() => dispatch(toggleTheme)}>Изменить тему оформления на {theme}</button>
-                <p>text text text text text text text text text text text text text text text text </p>
+                <button onClick={() => dispatch(toggleTheme())}>
+                    Изменить тему оформления на {theme === "light" ? "dark" : "light"}
+                </button>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat numquam nihil, vel non eaque
+                    consequuntur deleniti. Sit, impedit. Quas officiis molestiae laborum dolorum? Maiores beatae
+                    assumenda, ad, consequatur illo, eius deleniti fuga similique laudantium dolorem totam et saepe! Aut
+                    sunt esse animi temporibus impedit et dolor facilis minus, voluptas quod.
+                </p>
             </header>
         </div>
     );
--- homework05/src/store/themeReducer.js
+++ homework05/src/store/themeReducer.js
@@ -3,11 +3,10 @@ const TOGGLE_THEME = "TOGGLE_THEME";
 export const toggleTheme = () => ({ type: TOGGLE_THEME });
 
 const initialState = {
-    theme: "light",
+    theme: "dark",
 };
 
 const themeReducer = (state = initialState, action) => {
-    console.log(state.theme + " from themeReducer");
     switch (action.type) {
         case TOGGLE_THEME:
             return state.theme === "light" ? "dark" : "light";
